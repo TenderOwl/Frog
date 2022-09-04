06b0d31640bf6c7bab802c46cdd2fce11571b505
Andrey Maksimov <meamka@ya.ru>
2022-09-05 00:24:02 +0300

    Add autocopy setting

--- frog/clipboard_service.py
+++ frog/clipboard_service.py
@@ -1,12 +1,13 @@
-from gi.repository import Gdk
+from gi.repository import Gdk, GObject
 
 
 class ClipboardService:
     clipboard: Gdk.Clipboard = Gdk.Display.get_default().get_clipboard()
 
     @classmethod
-    def set(cls, value):
-        cls.clipboard.set(value)
+    def set(cls, value: str) -> None:
+        v = GObject.Value(GObject.TYPE_STRING, value)
+        cls.clipboard.set_content(Gdk.ContentProvider.new_for_value(v))
 
 
 clipboard_service = ClipboardService()
